a5bc1b0061be05b5a2ddf10d10a2ccec6e698827
Rutvik Vachhani <rutvik.vachhani@yahoo.com>
2021-12-21 07:41:42 +0530

    F to f

--- frontend/src/components/bmiInsert.js
+++ frontend/src/components/bmiInsert.js
@@ -4,7 +4,7 @@ import axios from "axios"
 import { useHistory } from "react-router-dom"
 
 import Nav from "./Nav";
-import Footer from "./Footer";
+import Footer from "./footer";
 
 const BMIInsert = () => {
 
